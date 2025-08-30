05a69eb5e13038f243f5f656245ac8c784536c96
Jaszolos Adam <adam.jaszolos@gmail.com>
2025-08-30 18:38:04 +0200

    change user to agerating

--- backend/app/Models/AgeRating.php
+++ backend/app/Models/AgeRating.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AgeRating extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\Database\Factories\AgeRatingFactory> */
     use HasFactory;
 
     /**
