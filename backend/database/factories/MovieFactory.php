f8328a88e38304166385a160e77e6df09404f920
Jaszolos Adam <adam.jaszolos@gmail.com>
2025-08-30 18:02:36 +0200

    create factories

---
+++ backend/database/factories/MovieFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AgeRating;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'age_rating_id' => AgeRating::factory(),
+        ];
+    }
+}
