8f0722a2635893f0f3bd3bcc8ba7c13708a0e18f
Jaszolos Adam <adam.jaszolos@gmail.com>
2025-08-30 11:27:59 +0200

    create models and migrations

---
+++ backend/app/Models/AgeRating.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class AgeRating extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'code',
+        'name',
+    ];
+
+    /**
+     * Get the movies for the age rating.
+     */
+    public function movies(): HasMany
+    {
+        return $this->hasMany(Movie::class);
+    }
+}
---
+++ backend/app/Models/Movie.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Movie extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'age_rating_id',
+    ];
+
+    /**
+     * Get the age rating that owns the movie.
+     */
+    public function ageRating(): BelongsTo
+    {
+        return $this->belongsTo(AgeRating::class);
+    }
+}
