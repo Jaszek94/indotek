6ffe54c019187c4993c1ebea1c920107972af97a
Jaszolos Adam <adam.jaszolos@gmail.com>
2025-08-30 16:22:05 +0200

    create movie service

---
+++ backend/app/Services/MovieService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\MovieRepositoryInterface;
+use App\Models\Movie;
+
+class MovieService
+{
+    public function __construct(
+        protected MovieRepositoryInterface $movieRepository
+    )
+    {
+        // Initialization if needed
+    }
+
+    public function list($filters = [])
+    {
+        return $this->movieRepository->all($filters);
+    }
+
+    public function find(int $id): Movie
+    {
+        return $this->movieRepository->find($id);
+    }
+
+    public function create(array $data): Movie
+    {
+        return $this->movieRepository->create($data);
+    }
+
+    public function update(Movie $movie, array $data): Movie
+    {
+        return $this->movieRepository->update($movie, $data);
+    }
+
+    public function delete(Movie $movie): void
+    {
+        $this->movieRepository->delete($movie);
+    }
+}
