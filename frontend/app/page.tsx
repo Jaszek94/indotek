52be2461c0e70a5ee7ecfc20cf89fd0f8acc3b85
Jaszolos Adam <adam.jaszolos@gmail.com>
2025-08-31 09:22:31 +0200

    design home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,14 +2,24 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col bg-gradient-to-br from-graybg-light to-graybg-dark">
-      Üdvj a próbafeladaton!
-      <Link
-        href="/admin/movies"
-        className="block px-3 py-2 rounded-md hover:bg-brand hover:text-white transition"
-      >
-        Go to Admin
-      </Link>
+    <div className="relative w-screen h-screen bg-gray-50 flex items-center justify-center overflow-hidden">
+      {/* Blur dekorációk */}
+      <div className="absolute top-[-10%] left-[-10%] w-72 h-72 bg-primary-light rounded-full filter blur-3xl opacity-30"></div>
+      <div className="absolute bottom-[-10%] right-[-10%] w-96 h-96 bg-primary-light rounded-full filter blur-3xl opacity-30"></div>
+
+      {/* Tartalom */}
+      <div className="relative z-10 text-center px-4">
+        <h2 className="text-4xl md:text-5xl font-bold text-gray-600 mb-6">
+          Jászolos Ádám próbafeladat
+        </h2>
+        <p className="text-gray-600 mb-6">Üdvözlöm a próbafeladaton!</p>
+        <Link
+          href="/admin"
+          className="inline-block px-8 py-3 bg-primary text-white rounded-lg shadow-lg hover:bg-primary-dark transition"
+        >
+          Admin
+        </Link>
+      </div>
     </div>
   );
 }
